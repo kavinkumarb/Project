3040a669033ea333f8a499906ed036f7c6e51264
kavinkumarb <kavinkumar0506@gmail.com>
2025-02-12 11:08:07 +0530

    stra87

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Bikes from "./pages/Bikes";
+import Accessories from "./pages/Accessories";
+import Services from "./pages/Services";
+import Customers from "./pages/Customers";
+import UpcomingBikes from "./pages/UpcomingBikes";
+import BikeDetails from "./pages/BikeDetails";
+import PurchasedBikes from "./pages/PurchasedBikes";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./App.css";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/bikeshowroom", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 function App() {
-  
   return (
-    <>
-      
-        
-      <h1> kavin-Bike-showroom</h1>
-      
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bikes" element={<Bikes />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/upcoming-bikes" element={<UpcomingBikes />} />
+        <Route path="/bike-details/:id" element={<BikeDetails />} />
+        <Route path="/purchased-bikes" element={<PurchasedBikes />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
