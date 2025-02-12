26458ed63e20b159c4428c69e242a7c57fcfea5b
kavinkumarb <kavinkumar0506@gmail.com>
2025-02-12 11:35:10 +0530

    bike

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,10 @@
-import { useState } from "react";
-import "./App.css";
-import NavBar from "./components/NavBar";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import SearchPage from "./pages/SearchPage";
-import ProfilePage from "./pages/ProfilePage";
-
-
 function App() {
-  const [activePageIndex, setActivePageIndex] = useState(0);
-
-  const NAVBAR_ITEMS = [
-    { icon: "🏠", text: "home", component: <HomePage /> },
-    { icon: "🖼️", text: "About", component: <AboutPage /> },
-    { icon: "🔍", text: "search", component: <SearchPage /> },
-    { icon: "🤦", text: "Profile", component: <ProfilePage /> },
-  ];
-
-  const activePage = NAVBAR_ITEMS[activePageIndex].component;
+  
 
   return (
-    <div className="app">
-      <NavBar ITEMS={NAVBAR_ITEMS} setActivePageIndex={setActivePageIndex} />
-      <div className="main-container">
-        {activePage}
-      </div>
-    </div>
+   <>
+   <h1>Hi iam kavin</h1>
+   </>
   );
 }
 
