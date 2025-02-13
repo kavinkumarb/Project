d72b9c05994d9fe75dd18f2102b98450295934e9
kavinkumarb <kavinkumar0506@gmail.com>
2025-02-13 16:22:49 +0530

    bike

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -3,7 +3,7 @@ import Page from "../components/Page";
 function SearchPage() {
     return (
         <Page>
-            <h1>Search page</h1>
+            <h1>Search page </h1>
         </Page>
     );
 }
